d0fbc4dafeb541988dad9860bad7d8f1aa1cb8c2
Pratik Raj <pratikraj26@gmail.com>
2017-04-02 16:32:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,14 +1,6 @@
 //  OpenShift sample Node application
 var express = require('express'),
-    fs      = require('fs'),
-    app     = express(),
-    eps     = require('ejs'),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
-
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+    app     = express();
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
